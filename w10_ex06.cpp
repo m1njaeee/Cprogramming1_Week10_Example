fa413e3eda73fe8f5227fe465e44a434324222ee
m1njaeee <alswo0867@naver.com>
2024-11-15 18:03:09 +0900

    Add files via upload

---
+++ w10_ex06.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+	int i = 10;
+	int* pi = &i;
+
+	printf("i = %d, pi = %p\n", i, pi);
+
+	(*pi)++;
+	printf("i = %d, pi = %p\n", i, pi);
+
+	*pi++;
+	printf("i = %d, pi = %p\n", i, pi);
+
+
+	return 0;
+}
+
